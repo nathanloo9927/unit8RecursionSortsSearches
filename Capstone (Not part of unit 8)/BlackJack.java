e94a5e2a49b50aaeaabdbe5ea64d0c2153132921
nathanloo9927 <NGLoo@student.naperville203.org>
2016-04-14 14:14:16 -0500

    41 (capstone day2)

--- Capstone (Not part of unit 8)/BlackJack.java
+++ Capstone (Not part of unit 8)/BlackJack.java
@@ -3,11 +3,46 @@ import java.awt.*;
 import javax.swing.*;
 public class BlackJack
 {
-    private int players;
+    private int numplayers;
     private int users;
-    public BlackJack(int players, int users)
+    private String[] players;
+    private ArrayList<Integer> cards = new ArrayList<Integer>(52);
+    public BlackJack(int numPlayers, int users, String[] names)
     {
-        this.players = players;
+        this.numplayers = numPlayers;
         this.users = users;
+        this.players = new String[numPlayers];
+        int comNamer = 1;
+        for (int i = 0; i < names.length; i++)
+        {
+            players[i] = names[i];
+        }
+        for (int i = names.length; i < numPlayers; i++)
+        {
+            players[i] = "COM " + comNamer;
+            comNamer++;
+        }
+    }
+    public String getName(int pos)
+    {
+        return players[pos];
+    }
+    public int deck()
+    {
+        return cards.size();
+    }
+    public void makeDeck()
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            for (int j = 1; j <= 13; j++)
+            {
+                cards.add(j);
+            }
+        }
+    }
+    public void removeAllCards()
+    {
+        cards.clear();
     }
 }
--- Capstone (Not part of unit 8)/BlackJackRunner.java
+++ Capstone (Not part of unit 8)/BlackJackRunner.java
@@ -8,13 +8,13 @@ public class BlackJackRunner
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to BlackJack");
         System.out.println("Choose the number of players that are playing, then choose how many");
-        System.out.println("of those players are controlled by a computer");
+        System.out.println("of those players are controlled by an actual user");
         int players;
         int users;
         do {
             System.out.print("How many players are playing? (2-10 players): ");
             while (!in.hasNextInt()) {
-                System.out.println("That's not a number!");
+                System.out.println("That's not a number or isn't an integer!");
                 System.out.print("How many players are playing? (2-10 players): ");
                 in.next();
             }
@@ -24,13 +24,37 @@ public class BlackJackRunner
             System.out.println("How many of those players will be controlled by a player (At least 1");
             System.out.print("player must be controlled by the user): ");
             while (!in.hasNextInt()) {
-                System.out.println("That's not a number!");
+                System.out.println("That's not a number or isn't an integer!");
                 System.out.println("How many of those players will be controlled by a player (At least 1");
                 System.out.print("player must be controlled by the user): ");
                 in.next();
             }
             users = in.nextInt();
         } while (users < 1 || users > players);
-        BlackJack game = new BlackJack(players, users);
+        String[] names = new String[users];
+        for (int i = 0; i < names.length; i++)
+        {
+            System.out.print("Enter the name for player " + (i + 1) + ": ");
+            names[i] = in.next();
+        }
+        String response = "yes";
+        boolean firstTime = true;
+        while (response.equals("yes"))
+        {
+            BlackJack game = new BlackJack(players, users, names);
+            if (firstTime == true)
+            {
+                System.out.println("\nThese are the people who will be playing:");
+                for (int i = 0; i < players; i++)
+                {
+                    System.out.println(game.getName(i));
+                }
+                firstTime = false;
+            }
+            game.makeDeck();
+            System.out.println(game.deck());
+            System.out.print("Play again? (yes/any other response = no): ");
+            response = in.next();
+        }
     }
 }
